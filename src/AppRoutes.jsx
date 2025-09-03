866fb496509bbd1239d3929b4d8f4e3877468977
dan-seng <da16gi@gmail.com>
2025-09-03 15:59:45 +0300

    Routing

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,16 +1,18 @@
 import { Route, Routes } from "react-router-dom";
-import Homepage from "./pages/Homepage";
 import Trending from "./pages/Trending";
 import Rating from "./pages/Rating";
 import Genre from "./pages/Genre";
+import Trailors from "./pages/Trailors";
+import MovieDetail from "./pages/MovieDetails";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/trending" element={<Trending />} />
+      <Route path="/" element={<Trending />} />
       <Route path="/rating" element={<Rating />} />
       <Route path="/genre" element={<Genre />} />
+      <Route path="/trailor" element={<Trailors />} />
+      <Route path="/movie/:id" element={<MovieDetail />} />
     </Routes>
   );
 }
