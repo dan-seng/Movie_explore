80d592a3c529047ab7d49b7351f4c6b65c03cec7
dan-seng <da16gi@gmail.com>
2025-09-03 15:55:16 +0300

    Update the App.jsx with necessary components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 
-import Homepage from "./pages/Homepage";
 import Navbar from "./components/Navbar";
 import AppRoutes from "./AppRoutes";
-
+import {HeroUIProvider} from "@heroui/react";
+import {ThemeProvider as NextThemesProvider} from "next-themes";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+     <HeroUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
      <Navbar/>
      <AppRoutes/>
+     <Footer/>
+    </NextThemesProvider>
+    </HeroUIProvider>
     </>
   );
 }
 
 export default App;
+
